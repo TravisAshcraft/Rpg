0d37937e25a5f51237c83365e2a3ee413b4495f5
TravisAshcraft <58603687+TravisAshcraft@users.noreply.github.com>
2020-02-23 22:28:21 -0600

    Added Cinemachine to follow camera added events with cinemachine and have used a observer pattern to print out when control has started and stopped.

---
+++ Assets/Scripts/Cinnematics/CinematicTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private bool isTriggered = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!isTriggered && other.gameObject.tag == "Player") 
+            {
+                isTriggered = true;
+                GetComponent<PlayableDirector>().Play();
+            }
+            
+        }
+    }
+}
